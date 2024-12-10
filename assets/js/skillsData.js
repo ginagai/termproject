e2abbf90056780f92c75ea757014212d227f5045
gina00-1 <wenligai@bu.edu>
2024-12-10 02:37:07 -0500

    Update skillsData.js

---
+++ assets/js/skillsData.js
@@ -0,0 +1,9 @@
+const skills = [
+    { name: "HTML5", level: "Advanced" },
+    { name: "CSS3", level: "Advanced" },
+    { name: "JavaScript", level: "Intermediate" },
+    { name: "React", level: "Intermediate" },
+    { name: "TypeScript", level: "Beginner" },
+];
+
+export default skills;
